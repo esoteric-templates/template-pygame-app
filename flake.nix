ba9e8bd2e6128f427015b2cafcbb5024d4abd3b0
Дима Ш. <dima.o.sh@proton.me>
2025-11-01 22:01:01 +0000

    Initialize the Python project
    * Initialized an easy-to-develop simple Python project that can also be built with Nix.

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "A template Python package";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      packages.${system}.default = pkgs.python3Packages.buildPythonPackage {
+        pname = "template";
+        version = "0.1.0";
+        src = ./.;
+
+        format = "pyproject";
+
+        nativeBuildInputs = with pkgs.python3Packages; [
+          setuptools
+          wheel
+        ];
+
+        meta = with pkgs.lib; {
+          description = "A template Python package";
+          license = licenses.agpl3Only;
+        };
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          python3
+          python3Packages.pip
+          python3Packages.virtualenv
+        ];
+
+        shellHook = ''
+          rm -rf .venv/
+          python -m venv .venv/
+
+          source .venv/bin/activate
+
+          pip install -r requirements.txt
+        '';
+      };
+    };
+}
